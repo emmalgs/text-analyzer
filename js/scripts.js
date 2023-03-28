3abacb26305b78c59cb1c1db298d27d896731a32
Emma Gerigscott <elgerig@gmail.com>
2023-03-28 11:14:24 -0700

    test solution for return 0 for empty string

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 function wordCounter(text) {
   let wordCount = 0;
   const textArray = text.split(" ");
+  if (textArray.length > 0) {
   textArray.forEach(function(word) {
     wordCount++;
-  });
+    });
+  } else if (textArray.length === 0) {
+    wordCount = 0;
+  }
   return wordCount;
 }
